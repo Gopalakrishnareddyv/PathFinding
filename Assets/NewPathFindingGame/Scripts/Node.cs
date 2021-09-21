c712abf6fde3e612d1393f8e9d4b43988e40dafe
GOPALAKRISHNAREDDY <gopalakrishna.vintha@xcubelabs.com>
2021-09-21 16:23:13 +0530

    created grid

---
+++ Assets/NewPathFindingGame/Scripts/Node.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node
+{
+    public bool walkable;
+    public Vector3 worldPosition;
+    //constructor 
+
+    public Node(bool _walkable,Vector3 _worldPosition)
+    {
+        walkable = _walkable;
+        worldPosition = _worldPosition;
+    }
+}
